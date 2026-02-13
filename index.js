2db2f7e23d6efa6bfed45982e70abce24467190a
AakashShukla18 <aakash.s@virtualizetechnologies.com>
2026-02-13 18:40:43 +0530

    intial commit

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const { createServer } = require('node:http');
+const { Server } = require('socket.io');
+const path = require('node:path');
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+app.use(express.static("client"));
+
+const players = {};
+
+io.on('connection', (socket) => {
+    console.log('User connected:', socket.id);
+
+    players[socket.id] = { x: 100, y: 100 };
+
+    socket.emit('currentPlayers', players);
+
+    socket.broadcast.emit('newPlayer', {
+        id: socket.id,
+        x: 100,
+        y: 100
+    });
+
+    // MOVE HANDLER MUST BE HERE
+    socket.on('move', (movementData) => {
+        if (!players[socket.id]) return;
+
+        players[socket.id].x = movementData.x;
+        players[socket.id].y = movementData.y;
+
+        io.emit('playerMoved', {
+            id: socket.id,
+            x: movementData.x,
+            y: movementData.y
+        });
+    });
+
+    socket.on('disconnect', () => {
+        delete players[socket.id];
+        io.emit('playerDisconnected', socket.id);
+    });
+});
+
+server.listen(3000, () => {
+    console.log('Server running on http://localhost:3000');
+});
